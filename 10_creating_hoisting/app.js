0bb7d1d6216c4d66e909b16dd0f8801d4e53162c
cuongdnq2710 <cuong.duong@titancorpvn.com>
2019-07-07 14:08:26 +0700

    10. creation & hoisting

---
+++ 10_creating_hoisting/app.js
@@ -0,0 +1,18 @@
+b() // Called b
+console.log(a) // undefined
+
+var a = 'Hello World'
+
+function b() {
+    console.log('Called b')
+}
+
+// --- Actual ----
+function b() { // move to top -> hoisting
+    console.log('Called b')
+}
+var a // move declaration to top and placeholder is 'undefined' -> hoisting
+
+b()
+console.log(a) // undefined
+a = 'Hello World'
